fd7813211e6815aefbc29bb796fa5dec9a20e024
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-01-28 16:21:45 +0000

    Implement authentication layout and screens; refactor app structure and prevent splash screen auto-hide

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Authlayout = () => {
+  return (
+    <View>
+      <Text>Authlayout</Text>
+    </View>
+  )
+}
+
+export default Authlayout
+
--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,15 @@ import { Text, StyleSheet, View, StatusBar } from 'react-native'
 import { Slot, Stack } from 'expo-router'
 import {Link} from 'expo-router'
 import "../styles/global.styles.css"
+import { SplashScreen } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync(); // Prevent the splash screen from auto-hiding
 
 export default function App() {
    {
     return (
       <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-black font-bold text-2xl">AORA!</Text>
+        <Text className="text-3xl font-pblack">AORA!</Text>
         
         <Link href="/profile" className="text-blue-500">Profile</Link>
         <StatusBar style="auto" />
