7925b3a033263a657f08a58a2fa1298d0613ba1a
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-01-29 21:33:44 +0000

    refactor: Revamp app layout with SafeAreaView and ScrollView; enhance visuals with new images and text

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,54 @@
-import { Text, StyleSheet, View, StatusBar } from 'react-native'
-import { Slot, Stack } from 'expo-router'
-import {Link} from 'expo-router'
+import { Text, StyleSheet, View, StatusBar, Image } from 'react-native'
+import { Link } from 'expo-router'
 import "../styles/global.styles.css"
 import { SplashScreen } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+import { ScrollView } from 'react-native';
+import { Images, images } from '../constants';
+
 
 SplashScreen.preventAutoHideAsync(); // Prevent the splash screen from auto-hiding
 
 export default function App() {
-   {
-    return (
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl font-pblack">AORA!</Text>
-        
-        <Link href="/home" className="text-blue-500">Home</Link>
-        <StatusBar style="auto" />
-      </View>
-    )
-  }
-  
+  return (
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full justify-center items-center h-full px-4'>
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+          />
+
+          <Image
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Discover Endless Possibilities with {''}
+              <Text className='text-secondary-200'> Aora </Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2.5  left-1/2 -translate-x-1/4"
+              resizeMode="contain"
+            />
+          </View>
+          {/* <Text className='text-white text-center mt-5'> */}
+          <Text className='text-sm font-pregular text-white text-center mt-5 mb-5'>
+          Where Creativity Meets Innovation: Embark on a Journey of Limitless
+          Exploration with Aora
+          </Text>
+          
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
