ba1fea2dbf4b196f7677d17b8824ec227cf105fa
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-01-29 23:24:54 +0000

    refactor: Enhance SignIn component layout with SafeAreaView and ScrollView; add logo and status bar

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,13 +1,27 @@
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { images } from '../../constants'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-lvh px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className="w-[115px] h-[35px]"
+            />
+            <Text className='text-2xl text-white font-semibold mt-10 font-psemibold'>login to Aora</Text>
+            
+
+        </View>
+      </ScrollView>
+      <StatusBar style='auto'/>
+    </SafeAreaView>
   )
 }
 
-export default SignIn  
-
+export default SignIn
